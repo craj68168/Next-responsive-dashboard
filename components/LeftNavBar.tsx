d5abed842aff8043a693e1066f215df6fb83d1ea
craj68168 <craj68168@gmail.com>
2022-09-23 15:20:27 +0545

    jsx done for left navbar

--- components/LeftNavBar.tsx
+++ components/LeftNavBar.tsx
@@ -1,7 +1,61 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
+import { faBookOpen, faTachometerAlt,faHeart,faCog,faSignOut,faRocket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const LeftNavBar = () => {
-  return <div>LeftNavBar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <h2>StreamLine</h2>
+      </div>
+      <div className={styles.wrapper}>
+        <ul>
+          <li>
+            <FontAwesomeIcon
+              icon={faTachometerAlt}
+              style={{ width: "18px", cursor: "pointer" }}
+            />
+            <a href="#">Dashboard</a>
+          </li>
+          <li>
+            <FontAwesomeIcon
+              icon={faRocket}
+              style={{ width: "18px", cursor: "pointer" }}
+            />
+            <a href="#">Explore</a>
+          </li>
+          <li>
+            <FontAwesomeIcon
+              icon={faBookOpen}
+              style={{ width: "18px", cursor: "pointer" }}
+            />
+            <a href="#">Library</a>
+          </li>
+          <li>
+            <FontAwesomeIcon
+              icon={faHeart}
+              style={{ width: "18px", cursor: "pointer" }}
+            />
+            <a href="#">Favourite</a>
+          </li>
+          <li>
+            <FontAwesomeIcon
+              icon={faCog}
+              style={{ width: "18px", cursor: "pointer" }}
+            />
+            <a href="#">Setting</a>
+          </li>
+          <li>
+            <FontAwesomeIcon
+              icon={faSignOut}
+              style={{ width: "18px", cursor: "pointer" }}
+            />
+            <a href="#">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default LeftNavBar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import Content from "../components/Content";
 import Header from "../components/Header";
 import LeftNavBar from "../components/LeftNavBar";

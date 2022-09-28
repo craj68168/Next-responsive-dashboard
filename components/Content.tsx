67578591d4de630c2f67d6e11f7f064faf6387bb
craj68168 <craj68168@gmail.com>
2022-09-28 17:21:32 +0545

    charts ongoing

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,6 +1,62 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
+import {Line} from "react-chartjs-2"
+import { Doughnut } from "react-chartjs-2";
 const Content = () => {
+
+  //data for bar chart
+const data:any = {
+	labels: [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	],
+	datasets: [
+		{
+			label: "Sales/ month",
+			fill: true,
+			lineTension: 0.1,
+			backgroundColor: "rgba(75,192,192,0.4)",
+			borderColor: "rgba(75,192,192,1)",
+			borderCapStyle: "butt",
+			borderDash: [],
+			borderDashOffset: 0.0,
+			borderJoinStyle: "miter",
+			pointBorderColor: "rgba(75,192,192,1)",
+			pointBackgroundColor: "#fff",
+			pointBorderWidth: 0,
+			pointHoverRadius: 0,
+			pointHoverBackgroundColor: "rgba(75,192,192,1)",
+			pointHoverBorderColor: "rgba(220,220,220,1)",
+			pointHoverBorderWidth: 0,
+			pointRadius: 0,
+			pointHitRadius: 0,
+			data: [65, 59, 80, 81, 56, 55, 40, 57, 40, 48, 59, 62],
+		},
+	],
+};
+
+//doughnut chart data set
+
+const data1 = {
+	labels: ["Organic", "Social Media", "Websites"],
+	datasets: [
+		{
+			data: [300, 50, 100],
+			backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+			hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+		},
+	],
+};
   return (
     <div className={styles.contentcontainer}>
       <div className={styles.contentwrapper}>
@@ -28,9 +84,11 @@ const Content = () => {
       <div className={styles.charts}>
         <div className={styles.bar}>
           <h2>Sales</h2>
+          <Line data={data} width={400} height={400}/>
         </div>
         <div className={styles.circle}>
           <h2>Custom Arrived</h2>
+          <Doughnut data={data1} width={400} height={400}/>
         </div>
       </div>
     </div>
